2a84e83419524c88c8beb510e569803689fb5ad7
Timothy Lowe <TimothyLowe247@gmail.com>
2019-11-21 18:14:11 -0500

    adding uploadimage model to girl model

--- controllers/model.js
+++ controllers/model.js
@@ -2,20 +2,24 @@ const router = require("express").Router();
 const girl = require("../models/girl.js");
 
 router.post("/login", girl.login, (req, res)=>{
-	// console.log("user login", req)
+	console.log("Login MODEL End", req)
 
 	res.json({modelCreds:res.locals.modelCreds})
 })
 router.post("/register", girl.create, (req, res)=>{
 
 	const respdata = res.locals.modelCreds
-	console.log("MODEL register", respdata)
+	console.log("Register MODEL End", respdata)
 	
 	res.json({modelCreds: respdata})
 })
 router.put("/edit", girl.edit, (req, res)=>{
-	console.log("Put Request Model", req.body)
+	console.log("edit Request Model End", req.body)
 	res.json({resp: res.locals.editResp})
-	
+
+})
+router.post("/img_upload", (req, res)=>{
+	console.log("Upload Model End")
+	res.json("Upload Model Responce")
 })
 module.exports = router;
