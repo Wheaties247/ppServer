aa50cc57a56050d908ec01d1aee2102c2f4b6899
Timothy Lowe <TimothyLowe247@gmail.com>
2019-12-08 10:24:46 -0500

    added console log to log the body of the sent get request

--- controllers/webcam.js
+++ controllers/webcam.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 const webcam = require("../models/webcam.js");
 
 router.get("/:modelId", (req, res)=>{
-	console.log("Webcam Route")
+	console.log("Webcam Route", req.body)
+
 	res.json({msg: "connected"})
 })
 
